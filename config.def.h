05773a37dfbe3176aef040084c19bc54ad8b75c6
-hasty- <tikhomirovtimofei@gmail.com>
2021-08-03 16:32:36 +0300

    Set colors with pywal

--- config.def.h
+++ config.def.h
@@ -7,12 +7,21 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+#define wal "/home/hasty/.cache/wal/colors-wal-dmenu.h"
+
+#if __has_include(wal)
+#include wal
+#else
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+#endif
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
