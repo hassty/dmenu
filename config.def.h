ad1ee7022aad039c7d6031d3a061125885b4f5f1
-hasty- <tikhomirovtimofei@gmail.com>
2021-08-20 07:16:03 +0300

    Apply patch xresources-alt
    commit bfe15a33ae49a973cedc9404cecc74ebd83552b9
    Author: -hasty- <tikhomirovtimofei@gmail.com>
    Date:   Fri Aug 20 07:05:54 2021 +0300
    
        Apply patch xresources-alt

--- config.def.h
+++ config.def.h
@@ -5,17 +5,26 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xe4U;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
  "RobotoMono Nerd Font Mono:size=11",
  "JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char outfgcolor[]  = "#000000";
+static char outbgcolor[]  = "#00ffff";
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { outfgcolor,  outbgcolor},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -38,3 +47,14 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+	{ "outfgcolor",  STRING, &outfgcolor },
+	{ "outbgcolor",  STRING, &outbgcolor },
+};
